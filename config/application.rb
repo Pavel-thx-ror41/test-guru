517e15dd3c72725f5ba1aed0a8513f83fa32dab3
Pavel-thx-ror41 <todoer@yandex.ru>
2023-06-25 17:28:57 +0300

    DB time to UTC

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module TestGuru
     # in config/environments, which are processed later.
     #
     config.time_zone = "Moscow" # active_support/values/time_zone.rb
-    config.active_record.default_timezone = :local # https://learntutorials.net/ru/ruby-on-rails/topic/3367/изменение-часового-пояса-по-умолчанию
+    # https://learntutorials.net/ru/ruby-on-rails/topic/3367/изменение-часового-пояса-по-умолчанию
+    # config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
     #
     config.i18n.available_locales = [:ru]
