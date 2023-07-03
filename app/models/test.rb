28989e13a14132c29aba6000b2bf0993e627fc53
Pavel-thx-ror41 <todoer@yandex.ru>
2023-07-03 23:19:00 +0300

    add titles_by_category_title_desc to test

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,4 +3,15 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   has_many :passes
+
+  class << self
+    # отсортированный по убыванию массив названий Тестов с выбранной Категорией
+    def titles_by_category_title_desc(category_title)
+      joins(:category)
+        .where('categories.title': category_title)
+        .select('tests.title')
+        .order('tests.title DESC')
+        .pluck('tests.title')
+    end
+  end
 end
