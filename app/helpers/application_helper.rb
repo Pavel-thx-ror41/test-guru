84b80345f49db1b7ef8b26e33c2c8d735e01ca93
Pavel-thx-ror41 <todoer@yandex.ru>
2023-07-16 19:55:49 +0300

    add header and footer (adds)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def last_edition
+    2023
+  end
+
+  def github_url(author, repo)
+    link_to "исходник на GitHub: #{repo}", "https://github.com/#{author}/#{repo}", target: 'blank'
+  end
 end
