4159a3916c854b6cc2715fdaf8b23ce413f4ccbf
Pavel-thx-ror41 <todoer@yandex.ru>
2023-07-18 02:02:06 +0300

    add layout/page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,21 @@ module ApplicationHelper
   def to_ul(list)
     "<ul><li><mark>#{list.join('</li></mark><li><mark>')}</mark></li></ul>".html_safe
   end
+
+  def page_title
+    title = instance_variable_get("@#{controller_name.singularize}")&.title.to_s
+    case action_name
+    when 'index'
+      controller_name.capitalize
+    when 'new'
+      "#{action_name.capitalize} #{controller_name.singularize.capitalize}"
+    when 'edit'
+      "#{action_name.capitalize} #{controller_name.singularize.capitalize}: '#{title}'"
+    when 'show'
+      controller_name.singularize.capitalize
+      "#{controller_name.singularize.capitalize}: '#{title}'"
+    else
+      'Welcome'
+    end
+  end
 end
