8ffacf7ff5cd1af9b5e4f1fa9daa52aed6154c37
Pavel-thx-ror41 <todoer@yandex.ru>
2023-07-25 22:50:12 +0300

    add tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def entity_errors_list(entity)
     entity.errors.map { |e| [e.attribute, e.message].join(' ') }
   end
+
+  def current_user
+    @current_user ||= User.first
+  end
 end
---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,61 @@
+class TestsController < ApplicationController
+  before_action :set_test, only: %i[show edit update destroy]
+
+  def index
+    @tests = Test.all
+  end
+
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit; end
+
+  def create
+    @test = current_user.tests.new(test_params)
+
+    if @test.save
+      flash[:notice] = 'Test created'
+      redirect_to action: 'index'
+    else
+      flash.now[:notice] = { errors: entity_errors_list(@test) }
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      flash[:notice] = 'Test created'
+      redirect_to action: 'index'
+    else
+      flash.now[:notice] = { errors: entity_errors_list(@test) }
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    test = @test.destroy
+
+    flash[:notice] = if test.frozen? && !test.persisted?
+                       'Test was successfully destroyed.'
+                     else
+                       'Test was NOT destroyed.'
+                     end
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:category_id, :published, :level, :title, :info)
+  end
+
+  def set_test
+    @test = Test.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:notice] = 'Test not found'
+    redirect_to action: 'index'
+  end
+end
