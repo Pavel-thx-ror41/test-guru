afb0124b40fdcc27d932d0764c40acce41d92e1f
Pavel-thx-ror41 <todoer@yandex.ru>
2023-10-02 03:08:45 +0300

    test pass result

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,17 @@
 module TestPassagesHelper
+  def passage_questions_total
+    @test_passage.test.questions.count
+  end
+
+  def passage_questions_correct
+    @test_passage.correct_questions
+  end
+
+  def passage_questions_correct_percentage
+    (100.0 / passage_questions_total.to_f * passage_questions_correct.to_f).round(2)
+  end
+
+  def passage_questions_correct_answers_count_status
+    passage_questions_correct_percentage >= 85 ? 'passed' : 'failed'
+  end
 end
