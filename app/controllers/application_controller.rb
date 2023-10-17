6ea19d89877df4d6b7045cf7f22ce5108fd68f43
Pavel-thx-ror41 <todoer@yandex.ru>
2023-10-17 22:46:17 +0300

    feat: task 11.3 redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def entity_errors_list(entity)
     entity.errors.map { |e| [e.attribute, e.message].join(' ') }
   end
+
+  private
+
+  def after_sign_in_path_for(resource)
+    resource.is_a?(Admin) ? admin_tests_path : tests_path
+  end
 end
