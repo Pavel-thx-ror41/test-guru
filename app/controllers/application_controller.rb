f0945d457cb88ca1ef5f8911c9ae98545d5814ad
Pavel-thx-ror41 <todoer@yandex.ru>
2023-10-23 21:43:33 +0300

    fix: codereview user.admin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    resource.is_a?(Admin) ? admin_tests_path : tests_path
+    resource.admin? ? admin_tests_path : tests_path
   end
 
   protected
