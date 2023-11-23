a255357f64780c11bb8c299aa40645bc44da97b7
Pavel-thx-ror41 <todoer@yandex.ru>
2023-11-24 01:09:09 +0300

    add .env

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,6 +1,6 @@
 class GitHubClient
   ROOT_ENDPOINT = 'https://api.github.com/'
-  ACCESS_TOKEN = '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'
+  GITHUB_HTTP_ACCESS_TOKEN = ENV['GITHUB_HTTP_ACCESS_TOKEN']
 
   def initialize
     @http_client = setup_http_client
@@ -9,7 +9,7 @@ class GitHubClient
   def create_gist(params)
     @http_client.post('gists') do |request|
       request.headers['Accept'] = "application/vnd.github+json"
-      request.headers['Authorization'] = "Bearer #{ACCESS_TOKEN}"
+      request.headers['Authorization'] = "Bearer #{GITHUB_HTTP_ACCESS_TOKEN}"
       request.headers['X-GitHub-Api-Version'] =  "2022-11-28"
       request.headers['Content-Type'] = 'application/json'
 
