7cd300e11e30488cf07420a71ae777c25af0ba66
Pavel-thx-ror41 <todoer@yandex.ru>
2023-11-28 15:52:19 +0300

    fix: gist controller create

--- app/controllers/admin/gists_controller.rb
+++ app/controllers/admin/gists_controller.rb
@@ -6,25 +6,33 @@ class Admin::GistsController < Admin::BaseController
   end
 
   def create
-    new_github_gist = GistQuestionService.new(@test_passage.current_question).create
-    if new_github_gist.is_a? Array
-      new_gist_local_log = Gist.create!({
-                                          question: @test_passage.current_question,
-                                          user: current_user,
-                                          gist_url: new_github_gist[1]
-                                        })
+    new_github_gist_creation_result = GistQuestionService.new(@test_passage.current_question).create
+
+    if github_gist_created?(new_github_gist_creation_result)
+      new_gist_local_log = current_user.gists.create({ question: @test_passage.current_question,
+                                                       gist_url: new_github_gist_creation_result[1] })
     end
 
-    flash_options = if new_github_gist.is_a?(Array) && new_gist_local_log.persisted?
-                      { notice: "#{t('.success')}: #{helpers.link_to(*new_github_gist)}" }
+    flash_options = if gist_logged_locally?(new_gist_local_log)
+                      { notice: "#{t('.success')}: #{helpers.link_to(*new_github_gist_creation_result)}" }
+                    elsif github_gist_created?(new_github_gist_creation_result)
+                      { alert: "#{t('.failure')}: Gist created, but not logged locally" }
                     else
-                      { alert: "#{t('.failure')}: #{new_github_gist}" }
-                    end
+                      { alert: "#{t('.failure')}: #{new_github_gist_creation_result}" }
+    end
 
     redirect_to @test_passage, flash_options
   end
 
   private
+
+  def github_gist_created?(new_github_gist)
+    new_github_gist.is_a?(Array)
+  end
+
+  def gist_logged_locally?(new_gist_local_log)
+    new_gist_local_log.persisted?
+  end
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:test_passage_id])
